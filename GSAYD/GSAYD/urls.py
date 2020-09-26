3cf8dd3a336fc17206c4572c8ade9ac9fc90cf37
Prashant sagar <prashantsagar@192.168.0.105>
2020-09-26 23:25:59 +0530

    home were setup & login frontend created

--- GSAYD/GSAYD/urls.py
+++ GSAYD/GSAYD/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from  django.contrib import  admin
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
     path('events/', include('events.urls')),
     path('ebook/', include('ebook.urls')),
     path('course/', include('course.urls')),
