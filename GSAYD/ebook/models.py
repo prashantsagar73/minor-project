4c674075810bdd4f1cbba1a59666faefc3819108
Prashant sagar <prashantsagar@192.168.0.102>
2020-11-09 01:33:00 +0530

    club bugs fixed

--- GSAYD/ebook/models.py
+++ GSAYD/ebook/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Pdf(models.Model):
+    sno = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=110)
+    head = models.CharField(max_length=110)
+    timestamp = models.DateTimeField(auto_now_add=True, blank=True)
+    pdfile = models.ImageField(upload_to='media/pdfs',default="")
+
+    def __str__(self):
+        return 'Message from ' + self
+    #  this is for describing name and email in admin section(
--- GSAYD/ebook/views.py
+++ GSAYD/ebook/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse,redirect
-
+from .models import Pdf
 # Create your views here.
 def ebook(request):
+
     return render (request,'ebook.html')
